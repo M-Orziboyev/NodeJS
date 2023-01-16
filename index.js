3b23b63419c3da7fb37b79970a3ae631df6c6989
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-01-16 12:07:09 +0500

    Web server

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    //request method = Serverga so'rov 
-    //response method = Serverdan javob
-    console.log(req.url);
-    res.write('hello world');
-    res.end()
+    if (req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(`
+        <h2>send email</h2>
+        <form method="post" action="/">
+            <input type="text" name="email" placeholder="enter your email address"/>
+            <button type="submit">Send email</button>
+        </form>
+    `)
+    } else {
+        res.end('Email sent successfully')
+    }
 });
 
-server.listen(3000, ()=>{
+server.listen(3000, () => {
     console.log('listening on');
 });

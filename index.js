cbe6f715bb4cdee087b92f6ad42bd8c395fc1631
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-01-16 11:47:01 +0500

    nodemon scripts

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const events = require('events');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    //request method = Serverga so'rov 
+    //response method = Serverdan javob
+    console.log(req.url);
+    res.write('hello world');
+    res.end()
+});
+
+server.listen(3000, ()=>{
+    console.log('listening on');
+});
